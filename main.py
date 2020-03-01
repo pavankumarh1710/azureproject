ed566b0e8fc3bbd781bc3d5db892f6ea71411244
pavankumarh1710 <51759642+pavankumarh1710@users.noreply.github.com>
2020-03-01 15:40:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 from flask import Flask,render_template,request
-import sqlite3 as sql
-import pandas as pd
-import numpy as np
-application = app = Flask(__name__)
-import os
-#import redis
 
-con = sql.connect("database.db")
-#rd = redis.StrictRedis(host='shakthi8112.redis.cache.windows.net', port=6380, db=0,password='ncP8NFImWyXmxKjo1MOVoAmJg7KRFX7511MbiSFHR9k=',ssl=True)
+application = app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	print("hello")
   
-
-
-
 
 if __name__ == '__main__':
    app.run()
